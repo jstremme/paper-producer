4288ac8714fdad5dcbba464183bcf8ca50ba088c
jstremme <jstremme@gustavus.edu>
2022-03-20 17:55:21 -0700

    add initial paper producer class

--- README.md
+++ README.md
@@ -4,10 +4,36 @@ Produces new papers from old papers
 
 ### About
 
+Produces new papers from old papers by applying rules or ML models.
+
 ### Disclaimer
+
+For now, all derivative papers belong to the authors of the original paper.  
 
 ### Environment
 
-### Examples
+To build the `paper-producer` Python 3.8 environment, create a Python 3.8 virtual environment and install the dependencies in `requirements.txt`.  We recommend using [Anaconda](https://www.anaconda.com/products/individual) to create your environment.
+
+```
+git clone https://github.com/jstremme/paper-producer
+conda create --name=paper=producer python=3.8
+source activate paper-producer
+pip install -r requirements.txt
+```
+
+### Run Paper Producer
+
+To run `paper-producer`, follow these steps to activate your conda environment and run `paper_producer.py` with an arxiv paper ID provided as the only required argument.
+
+```
+source activate paper-producer
+python paper_producer --arxiv_id=<arxiv-paper-id-here>
+```
 
 ### Contributing
+
+To contribute features, bug fixes, tests, examples, or documentation, please submit a pull request with a description of your proposed changes or additions.
+
+Please include a brief description of your pull request when submitting code and ensure that your code follows the [Pep 8](https://www.python.org/dev/peps/pep-0008/) style guide.  To do this run `pip install black` and `black paper-producer` to reformat files within your copy of the code using the [black code formatter](https://github.com/psf/black).  The black code formatter is a PEP 8 compliant, opinionated formatter that reformats entire files in place.
+
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
---
+++ paper_producer.py
@@ -0,0 +1,90 @@
+import os
+import arxiv
+import argparse
+from pdfminer.high_level import extract_text
+
+class PaperProducer(object):
+    """Produces Papers"""
+
+    def __init__(self, papers_dir='./papers', old_papers_dir='./', new_papers_dir='./'):
+        super(PaperProducer, self).__init__()
+        self.arxiv_id = None
+        self.old_paper_results = None
+        self.old_paper_text = None
+        self.new_paper_text = None
+        self.new_paper = None
+        self.papers_dir = papers_dir
+        self.old_papers_dir = old_papers_dir
+        self.new_papers_dir = new_papers_dir
+        self.setup()
+
+    def setup(self):
+
+        self.make_dir(dir=self.papers_dir)
+        self.make_dir(dir=self.old_papers_dir)
+        self.make_dir(dir=self.new_papers_dir)
+
+    def make_dir(self, dir):
+
+        if os.path.exists(dir):
+            os.makedirs(dir)
+
+    def download_paper(self, arxiv_id):
+
+        self.arxiv_id = arxiv_id
+        search = arxiv.Search(id_list=[self.arxiv_id])
+        self.old_paper_results = next(search.results())
+        self.old_paper_results.download_pdf(dirpath=self.old_papers_dir, filename=f"{arxiv_id}.pdf")
+
+    def pdf2text(self):
+
+        self.old_paper_text = extract_text(os.path.join(self.old_papers_dir, f"{arxiv_id}.pdf"))
+
+    def produce_paper_text(self):
+        """Find and replace keywords according to Mike
+        implement core replacement logic in another method"""
+
+        self.new_paper_text = self.old_paper_text
+
+    def format_paper_text(self):
+        """Format new paper text to be nice"""
+
+        pass
+
+    def save_new_paper(self):
+        """Save new paper"""
+
+        pass
+
+    def produce_paper_from_arxiv_paper(self, arxiv_id):
+
+        self.download_paper(self.arxiv_id)
+        self.pdf2text()
+        self.produce_paper_text()
+        self.format_paper_text()
+        self.save_new_paper()
+
+def parse_arguments(parser):
+    """Read user arguments"""
+
+    parser.add_argument(
+        "--arxiv_id", type=str, required=True, help="arxiv paper ID to use to produce a new paper"
+    )
+
+    args = parser.parse_args()
+
+    return args
+
+if __name__ == '__main__':
+
+    # Build arg parser and parse args
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    args = parse_arguments(parser)
+
+    # Initialize paper producer
+    paper_producer = PaperProducer()
+
+    # Produce paper from supplied arxiv paper ID
+    paper_producer.produce_paper_from_arxiv_paper(arxiv_id=args.arxiv_id)
